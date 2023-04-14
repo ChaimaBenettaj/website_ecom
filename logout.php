df9eed8ab3588469ffb20fad6e71c9c06eca242c
El Mehdi Benennia <elmehdibenennia@gmail.com>
2023-04-14 11:55:38 +0000

    delete all files

--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once(__DIR__."/_includes/utils/_init.php");
-    require_once(__DIR__."/_includes/models/user.php");
-    User::logout();
-    header("Location: ".PROJECT_URL."/");
